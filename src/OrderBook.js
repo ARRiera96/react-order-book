fde03c07e8414a31222214c74dc21fe70ba784c0
Angel Riera <rriera96@gmail.com>
2019-11-23 13:42:30 -0500

    Sorting asks and rendering bookside component

--- src/OrderBook.js
+++ src/OrderBook.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line
 import React, { useState, useEffect } from 'react';
 import KrakenClient from 'kraken-api';
+import BookSide from "./BookSide";
 const key          = ''; // API Key
 const secret       = ''; // API Private Key
 const kraken       = new KrakenClient(key, secret);
@@ -10,9 +11,12 @@ const OrderBook = () => {
 
     async function requestOrderBook() {
         let pair = 'XETHZUSD';
-        const { result } = await kraken.api('Depth', { pair : pair });
+        const { result } = await kraken.api('Depth', { pair : pair , count: 25});
         // const { asks, bids } = result[pair];
         const { asks } = result[pair];
+        asks.sort((a,b) => {
+            return b[0] - a[0];
+        });
         setAsks(asks || []);
         console.log( asks);
     }
@@ -24,8 +28,10 @@ const OrderBook = () => {
     return (
         <div className="bg-dark">
             <h2 className="text-light">Table Placeholder</h2>
+            <BookSide asks={asks}/>
         </div>
     );
 };
 
 export default OrderBook;
+
