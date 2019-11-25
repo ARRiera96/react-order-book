873b58c741d9fa3c6406c984705e30bb24b068ab
Angel Riera <rriera96@gmail.com>
2019-11-24 23:11:05 -0500

    Updates

---
+++ precache-manifest.d43900c0bcdc6a272a7e8287b18b4485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8c11d1a553661c8327a7f746f2f9b5",
+    "url": "/react-order-book/index.html"
+  },
+  {
+    "revision": "1b2890b806cadde34f5e",
+    "url": "/react-order-book/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c2b4298222f50e5312b6",
+    "url": "/react-order-book/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1b2890b806cadde34f5e",
+    "url": "/react-order-book/static/js/2.168a0991.chunk.js"
+  },
+  {
+    "revision": "c2b4298222f50e5312b6",
+    "url": "/react-order-book/static/js/main.8106b9e7.chunk.js"
+  },
+  {
+    "revision": "99a1757ea4631652b9c7",
+    "url": "/react-order-book/static/js/runtime-main.864f02a6.js"
+  }
+]);
